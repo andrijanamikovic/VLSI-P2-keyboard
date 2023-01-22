f371ffe72ba9b5a00ced60452955e70a043098de
Andrijana Mikovic <andrijana.mikovic00@gmail.com>
2023-01-22 01:07:44 +0100

    Doesn't work

--- src/simulation/modules/ps2.v
+++ src/simulation/modules/ps2.v
@@ -17,45 +17,45 @@ module ps2(
     reg [3:0] cnt_reg, cnt_next; //bits red
     reg ps2clk_reg, ps2clk_next;
 
-
-
-
     reg[15:0] hex_code_reg, hex_code_next;
-    wire[7:0] data_in; //trenutni kod
     wire[15:0] hex_in;
-    assign neg_edge = ps2clk_reg & ~ps2clk_next;
-    assign data_in = buffer_reg[8:1];
-    assign hex_in = hex_code_reg;
-    assign out = hex_in;
+    reg neg_edge;
+    // assign neg_edge = ps2clk_reg & ~ps2clk_next;
+    assign code = hex_code_reg;
 
     integer i;
     reg parity;
 
     always @(posedge clk, negedge rst_n) begin
         if (!rst_n) begin
-            hex_code_reg <= 16'h0000;
+            hex_code_reg <= 16'hffff;
             buffer_reg <= 10'h000;
             state_reg <= 1'b0;
-            cnt_reg <= 4'd0;
+            cnt_reg <= 4'd9;
             ps2clk_reg <= 1'b0;
-            
-        end        
-
-        else begin
+            //ps2clk_next <= 1'b0;
+        end else begin
+            //ps2clk_next <= ps2clk;
             hex_code_reg <= hex_code_next;
             buffer_reg <= buffer_next;
             state_reg <= state_next;
             cnt_reg <= cnt_next;
             ps2clk_reg <= ps2clk_next; 
-        end
-            
+        end            
     end
     
-    always @(neg_edge) begin
+    always @(*) begin
+        
         state_next = state_reg;
         buffer_next = buffer_reg;
         cnt_next = cnt_reg;
         ps2clk_next = ps2clk;
+        neg_edge = ps2clk_reg & ~ps2clk_next;
+        // buffer_next[4'b1001 - cnt_reg] = ps2data;
+        // cnt_next = cnt_reg - 4'h1;
+        //hex_code_next = hex_code_reg;
+        //hex_code_next = {8'h11, buffer_next[7:0]}; 
+        
         case (state_reg)
             idle: begin
                 if (neg_edge) begin
@@ -65,23 +65,30 @@ module ps2(
                         cnt_next = 4'b1001;
                         state_next = read;
                         parity = 1'b0;
+                        hex_code_next = 16'h5555;
                     end else state_next = idle;
+                end else begin
+                    hex_code_next = {ps2clk_reg, ps2clk_next, neg_edge, 13'h1fff};
                 end
             end
                 
             read: begin
+                //hex_code_next = {ps2clk_reg, ps2clk_next, neg_edge, 13'h0000};
                 if(neg_edge) begin
                     buffer_next[4'b1001 - cnt_reg] = ps2data;
-                    cnt_next = cnt_reg - 1;
+                    cnt_next = cnt_reg - 4'h1;
                 end
                 if (cnt_reg == 4'h0) begin
-                    for (i = 0 ; i < 8; i = i + 1 ) begin
-                        parity = parity ^ buffer_next[i];
-                    end
+                    hex_code_next = {8'h00, buffer_next[7:0]};
+                    // for (i = 0 ; i < 8; i = i + 1 ) begin
+                    //     parity = parity ^ buffer_next[i];
+                    // end
                    
-                    if (parity == buffer_next[9]) begin
-
-                        if (buffer_next[7:0] == old_value_next[7:0]) begin
+                    // if (parity == buffer_next[9]) begin
+                        if (old_value_next[7:0] == 8'h00) begin
+                            hex_code_next = {8'h11, buffer_next[7:0]};  //kad se jednom pritisne od 1
+                        end
+                        else if (buffer_next[7:0] == old_value_next[7:0]) begin
                             hex_code_next = {8'h00, buffer_next[7:0]};  //kad se dugo drzi od 1B
                         end else if((old_value_next[7:0] == 8'he0) && (buffer_next[7:0] == 8'hf0)) begin
                             hex_code_next = hex_code_reg; //Kad se otpusti od dva da preskoci takt
@@ -95,7 +102,7 @@ module ps2(
                             hex_code_next = {old_value_next[7:0], buffer_next[7:0]}; //Drugi B koda od 2B otpusni
                          end
                         
-                    end
+                    // end
                     state_next = idle;
                 end
             end
